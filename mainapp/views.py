c3471a2252ca18b69f39e3e25fcb2d9d1fd53f75
Mohsen Bagheri <moh3nmx@gmail.com>
2021-07-15 01:05:28 +0430

    init

---
+++ mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from main import process_data
+# Create your views here.
+
+
+def index(request):
+    if request.FILES:
+        import pandas as pd
+        csv = request.FILES['file']
+        data = pd.read_csv(csv)
+        output = process_data(data)
+        return render(request, 'results.html', {
+            'output': output
+        })
+    return render(request, 'index.html')
